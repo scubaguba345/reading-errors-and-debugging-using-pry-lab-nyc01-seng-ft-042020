74f6200b2203c2935751e305dc6ecbbbd228f1e7
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-31 23:25:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,9 @@ end
 
 def selection(num)
   if num = 1
-    "HAM HAM HAM IN MY TUMMY"
-elsif num = 2
     "YUM YUM MUNCH MUNCH MUNCH"
-
+elsif num = 2
+    "HAM HAM HAM IN MY TUMMY"
   end
 end
 
