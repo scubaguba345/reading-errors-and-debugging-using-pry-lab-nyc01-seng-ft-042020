0417bae92284c230d850b778d98be8ddd32e376e
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-31 23:23:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,10 +13,11 @@ end
 
 def selection(num)
   if num = 1
-    "YUM YUM MUNCH MUNCH MUNCH"
-  binding.pry
-  elsif num = 2
     "HAM HAM HAM IN MY TUMMY"
+  binding.pry
+elsif num = 2
+    "YUM YUM MUNCH MUNCH MUNCH"
+
   end
 end
 
